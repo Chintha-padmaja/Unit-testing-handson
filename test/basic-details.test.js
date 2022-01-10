2e7f181cc0adb013bb3e1e6c49e516b821a91636
Chintha-padmaja <nagapadmajachintha@gmail.com>
2022-01-10 11:35:55 +0000

    worked on _numToWord function testcase in basic details file

--- test/basic-details.test.js
+++ test/basic-details.test.js
@@ -1,6 +1,8 @@
 import { html, fixture, expect } from '@open-wc/testing';
+import { Required, MinNumber, MaxNumber } from '@lion/form-core';
 import Sinon from 'sinon';
 import '../src/LoanBasicDetails/BasicDetails.js';
+import { inWords } from '../src/utils/numToWord.js';
 
 const el = await fixture(html`<basic-details></basic-details>`);
 const form = el.shadowRoot.querySelectorAll('lion-button');
@@ -21,5 +23,28 @@ describe('Basic details', () => {
     form[1].click();
     expect(fun.calledOnce).to.be.true;
   });
+
+  it('check for empty field validation', () => {
+    const type = el.shadowRoot.querySelector('lion-input-amount');
+    const amount =  new Required();
+    const errorMessage = amount._getMessage();
+    // console.log(type);
+    // type.modelValue = null;
+    // console.log(type);
+    // const input = type.querySelector('[slot = "feedback"]').shadowRoot.innerHTML;
+    // console.log(input);
+     expect(errorMessage).to.not.equal();
+    // expect(errorMessage).to.equal("Amount is a required field");
+  });
+
+  it('checks for spy', () => {
+    const val = 10005;
+    let num = 10005;
+    const func =  el._numToWord(val,num) ;
+    console.log(func);
+    expect(num).to.equal(10005.00);
+  });
+
+
 
 });
